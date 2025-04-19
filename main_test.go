e5d0a5c1e1319a0a3bdf3993a4c43d85c074dbff
Anis Jonischkeit <anis.jonischkeit@gmail.com>
2025-04-19 19:33:47 +1000

    better logging

--- main_test.go
+++ main_test.go
@@ -9,7 +9,7 @@ import (
 func TestMetadataQuery(t *testing.T) {
     query := "LIST FROM \"examples/\" WHERE [author] IS Shakespeare"
     expectedOutput := "- shakespeare_quotes.md" 
-    msg, _ := executeQuery(query, true)
+    msg, _ := executeQuery(query, false)
     if msg != expectedOutput {
       t.Error("\n" + query + "\nis expected to output:\n" + expectedOutput + "\n Got \n" + msg)
     }
