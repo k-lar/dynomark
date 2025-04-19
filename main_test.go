6eab53ba29d33142e8e9c0a430a9d6e231615176
Anis Jonischkeit <anis.jonischkeit@gmail.com>
2025-04-19 19:29:11 +1000

    add test

---
+++ main_test.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+    "testing"
+)
+
+// TestHelloName calls greetings.Hello with a name, checking
+// for a valid return value.
+func TestMetadataQuery(t *testing.T) {
+    query := "LIST FROM \"examples/\" WHERE [author] IS Shakespeare"
+    expectedOutput := "- shakespeare_quotes.md" 
+    msg, _ := executeQuery(query, true)
+    if msg != expectedOutput {
+      t.Error("\n" + query + "\nis expected to output:\n" + expectedOutput + "\n Got \n" + msg)
+    }
+}
